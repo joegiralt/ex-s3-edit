de47bcf068394345ded5fa100eda1cead7e7cc01
Joseph Martin Giralt <joe.m.giralt@gmail.com>
2021-10-30 00:33:15 -0400

    update circle.yml update elixir version

--- mix.exs
+++ mix.exs
@@ -7,7 +7,7 @@ defmodule ExS3Edit.MixProject do
     [
       app: @app,
       version: "0.1.0",
-      elixir: "~> 1.13.0-dev",
+      elixir: "~> 1.12.3",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
       releases: [{@app, release()}],
